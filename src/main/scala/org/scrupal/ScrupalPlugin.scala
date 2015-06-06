ff2147971d6b3f9169f7d9a39586e29cfcecfef4
Reid Spencer <reid@reactific.com>
2015-06-06 17:36:04 -0400

    Modularize the settings to keep it DRY. Get working with Scrupal seed template.

--- src/main/scala/org/scrupal/ScrupalPlugin.scala
+++ src/main/scala/org/scrupal/ScrupalPlugin.scala
@@ -1,11 +1,9 @@
 package scrupal.sbt
 
-
 import scala.language.postfixOps
 
 import sbt._
 import sbt.Keys._
-import sbt.complete.Parsers._
 
 import com.typesafe.sbt.{GitVersioning, GitPlugin, JavaVersionCheckPlugin}
 import com.typesafe.sbt.JavaVersionCheckPlugin.autoImport._
@@ -14,18 +12,55 @@ import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
 
 import de.heikoseeberger.sbtheader.HeaderPlugin
 
-import play.sbt.{Play => PlayPlugin, PlayScala}
+import play.sbt.PlayScala
 
 import sbtsh.ShPlugin
+
+trait PluginSettings {
+  /** The [[Configuration]]s to add to each project that activates this AutoPlugin.*/
+  def projectConfigurations: Seq[Configuration] = Nil
+
+  /** The [[Setting]]s to add in the scope of each project that activates this AutoPlugin. */
+  def projectSettings: Seq[Setting[_]] = Nil
+
+  /** The [[Setting]]s to add to the build scope for each project that activates this AutoPlugin.
+    * The settings returned here are guaranteed to be added to a given build scope only once
+    * regardless of how many projects for that build activate this AutoPlugin. */
+  def buildSettings: Seq[Setting[_]] = Nil
+
+  /** The [[Setting]]s to add to the global scope exactly once if any project activates this AutoPlugin. */
+  def globalSettings: Seq[Setting[_]] = Nil
+}
 
 /** The ScrupalPlugin For Scrupal Based Modules */
 object ScrupalPlugin extends AutoPlugin {
 
+  val autoplugins : Seq[AutoPlugin] = Seq( PlayScala, JavaAppPackaging, JavaVersionCheckPlugin,
+    GitPlugin, HeaderPlugin, SbtBundle, ShPlugin)
+
+  override def requires = autoplugins.foldLeft(empty) { (b,plugin) => b && plugin }
+
   // Not AutoPlugins Yet: Scalariform, Unidoc, SbtSite, SbtGhPages, SbtUnidocPlugin :(
 
-  override def requires =
-    PlayScala && JavaAppPackaging && GitPlugin && HeaderPlugin && SbtBundle &&
-    JavaVersionCheckPlugin && ShPlugin && GitVersioning
+  /** Settings For Plugins that are not yet AutoPlugins so we can mimic them.
+    * This trait provides the same settings methods as an AutoPlugin. See [[sbt.AutoPlugin]]
+    * This is used to override settings in both AutoPlugins and regular Plugins.
+    */
+  val pluginSettings : Seq[PluginSettings] = Seq(
+    Compiler, Settings, Bundle, Scalariform, Unidoc, SonatypePublishing, Site, GhPages
+  )
+
+
+  object Compiler extends PluginSettings {
+    override def projectSettings : Seq[Setting[_]] = Seq(
+      scalaVersion := "2.11.6",
+      javaOptions in test ++= Seq("-Xmx512m"),
+      scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation", "-target:jvm-1.8"),
+      scalacOptions in(Compile, doc) ++=
+        Seq("-feature", "-unchecked", "-deprecation", "-diagrams", "-implicits", "-skip-packages", "samples")
+
+    )
+  }
 
   override def trigger = noTrigger
 
@@ -34,11 +69,15 @@ object ScrupalPlugin extends AutoPlugin {
    * when the plugin is enabled
    */
   object autoImport {
-    val scrupalCompileOnly = inputKey[Unit]("Compile just the specified files")
     val scrupalTitle = settingKey[String]("A title for the Scrupal module for use in documentation")
     val scrupalCopyrightHolder = settingKey[String]("The name of the copyright holder for the scrupal module")
     val scrupalCopyrightYears = settingKey[Seq[Int]]("The years in which the copyright was in place")
     val scrupalDeveloperUrl = settingKey[URL]("The URL for the developer's home page")
+
+    val compileOnly = TaskKey[File]("compile-only", "Compile just the specified files")
+
+    val printClasspath = TaskKey[File]("print-class-path", "Print the project's class path line by line.")
+    val printRuntimeClasspath = TaskKey[File]("print-runtime-class-path", "Print the project's runtime class path.")
   }
 
   val scrupalResolvers = Seq(
@@ -55,43 +94,62 @@ object ScrupalPlugin extends AutoPlugin {
   /**
    * Define the values of the settings
    */
-  override def projectSettings: Seq[Setting[_]] = PlayPlugin.projectSettings ++ Defaults.coreDefaultSettings ++
-    Compiler.projectSettings ++
-    Scalariform.projectSettings ++
-    Unidoc.projectSettings ++
-    SonatypePublishing.projectSettings ++
-    Site.projectSettings ++
-    GhPages.projectSettings ++
+  override def projectSettings: Seq[Setting[_]] = Defaults.coreDefaultSettings ++
+    autoplugins.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.projectSettings } ++
+    pluginSettings.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.projectSettings } ++
     Seq(
-      organization := "",
       resolvers := scrupalResolvers,
+      javaVersionPrefix in javaVersionCheck := Some("1.8"),
       ivyScala  := ivyScala.value map { _.copy(overrideScalaVersion = true) },
-      javaVersionPrefix in javaVersionCheck := Some("1.8")
+      printClasspath <<= Commands.print_class_path,
+      printRuntimeClasspath <<= Commands.print_runtime_class_path,
+      compileOnly <<= Commands.compile_only
     )
 
-  override def buildSettings : Seq[Setting[_]] = Defaults.buildCore
-  override def globalSettings: Seq[Setting[_]] = Seq(
-    compileOnlySetting
-  )
+  override def buildSettings : Seq[Setting[_]] = Defaults.buildCore ++
+    Seq(
+      baseDirectory := thisProject.value.base,
+      target := baseDirectory.value / "target"
+    ) ++
+    autoplugins.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.buildSettings } ++
+    pluginSettings.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.buildSettings } ++
+    Seq()
 
-  def compileOnlySetting: Setting[_] = scrupalCompileOnly := {
-    val args: Seq[String] = spaceDelimited("<arg>").parsed
-    args foreach println
+  override def globalSettings: Seq[Setting[_]] =
+    autoplugins.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.globalSettings } ++
+    pluginSettings.foldLeft(Seq.empty[Setting[_]]) { (s,p) => s ++ p.globalSettings } ++
+    Seq(
+    )
+}
+
+object Commands {
+  addCommandAlias("tq", "test-quick")
+  addCommandAlias("to", "test-only")
+
+  def print_class_path = (target, fullClasspath in Compile, compile in Compile) map { (out, cp, analysis) =>
+    println("----- " + out.getCanonicalPath + ": FILES:")
+    println(cp.files.map(_.getCanonicalPath).mkString("\n"))
+    println("----- " + out.getCanonicalPath + ": All Binary Dependencies:")
+    println(analysis.relations.allBinaryDeps.toSeq.mkString("\n"))
+    println("----- END")
+    out
   }
+
+  def print_runtime_class_path = (target, fullClasspath in Runtime).map { (out, cp) =>
+    println("----- " + out.getCanonicalPath + ": FILES:")
+    println(cp.files.map(_.getCanonicalPath).mkString("\n"))
+    println("----- END")
+    out
+  }
+
+  def compile_only = (target, compile in Compile) map { (out, compile) =>
+    println("Not Implemented Yet.")
+    out
+  }
+
 }
 
-object Compiler {
-  lazy val projectSettings : Seq[Setting[_]] = Defaults.coreDefaultSettings ++ Seq(
-    scalaVersion := "2.11.6",
-    javaOptions in test ++= Seq("-Xmx512m"),
-    scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation", "-target:jvm-1.8"),
-    scalacOptions in(Compile, doc) ++=
-      Seq("-feature", "-unchecked", "-deprecation", "-diagrams", "-implicits", "-skip-packages", "samples")
-
-  )
-}
-
-object Settings {
+object Settings extends PluginSettings {
 
   import ScrupalPlugin.autoImport._
 
@@ -102,7 +160,7 @@ object Settings {
     }
   }
 
-  val buildSettings: Seq[sbt.Def.Setting[_]] = Defaults.coreDefaultSettings ++
+  override def projectSettings : Seq[sbt.Def.Setting[_]] = Defaults.coreDefaultSettings ++
     Seq(
       scalacOptions in(Compile, doc) ++= Opts.doc.title(scrupalTitle.value),
       scalacOptions in(Compile, doc) ++= Opts.doc.version(version.value),
@@ -151,18 +209,18 @@ object ShellPrompt {
   }
 }
 
-trait PluginSettings {
-  /** The [[sbt.Setting]]s to add in the scope of each project that activates this AutoPlugin. */
-  def projectSettings: Seq[Setting[_]] = Nil
+object Bundle extends PluginSettings {
 
-  /** The [[sbt.Setting]]s to add to the build scope for each project that activates this AutoPlugin.
-    * The settings returned here are guaranteed to be added to a given build scope only once
-    * regardless of how many projects for that build activate this AutoPlugin. */
-  def buildSettings: Seq[Setting[_]] = Nil
+  import SbtBundle.autoImport.BundleKeys._
+  import com.typesafe.sbt.bundle.Import.ByteConversions.IntOps
 
-  /** The [[sbt.Setting]]s to add to the global scope exactly once if any project activates this AutoPlugin. */
-  def globalSettings: Seq[Setting[_]] = Nil
+  override def projectSettings : Seq[Setting[_]] = Seq(
+    memory := IntOps(1).GiB,
+    diskSpace := IntOps(1).GiB,
+    nrOfCpus := 2.0
+  )
 }
+
 
 object SonatypePublishing extends PluginSettings {
 
@@ -177,7 +235,12 @@ object SonatypePublishing extends PluginSettings {
     Some(resolver)
   }
 
-  override lazy val projectSettings = Sonatype.sonatypeSettings ++ Seq(
+  val defaultScmInfo = Def.setting {
+    val gitUrl = "//github.com/scrupal/" + normalizedName.value + ".git"
+    ScmInfo(url("https:" ++ gitUrl), "scm:git:" ++ gitUrl, Some("https:" ++ gitUrl) )
+  }
+
+  override def projectSettings = Sonatype.sonatypeSettings ++ Seq(
     Sonatype.SonatypeKeys.profileName := "org.scrupal",
     publishMavenStyle := true,
     publishArtifact in Test := false,
@@ -186,8 +249,8 @@ object SonatypePublishing extends PluginSettings {
     homepage := Some(new URL("http://scrupal.org/modules/" + normalizedName.value)),
     pomExtra :=
       <scm>
-        <url>{scmInfo.value.get.browseUrl.toString}</url>
-        <connection>{scmInfo.value.get.connection}</connection>
+        <url>{scmInfo.value.getOrElse(defaultScmInfo.value).browseUrl.toString}</url>
+        <connection>{scmInfo.value.getOrElse(defaultScmInfo.value).connection}</connection>
       </scm>
       <developers>
         <developer>
@@ -199,7 +262,6 @@ object SonatypePublishing extends PluginSettings {
   )
 }
 
-// import scala.language.postfixOps
 
 object Scalariform extends PluginSettings {
 
@@ -208,7 +270,7 @@ object Scalariform extends PluginSettings {
   import scalariform.formatter.preferences._
 
 
-  override lazy val projectSettings = scalariformSettings ++ Seq(
+  override def projectSettings = scalariformSettings ++ Seq(
     ScalariformKeys.preferences := formattingPreferences
   )
 
@@ -240,21 +302,43 @@ object Scalariform extends PluginSettings {
 object Unidoc extends PluginSettings {
   import sbtunidoc.{Plugin => UnidocPlugin}
   import ScrupalPlugin.autoImport._
-  override lazy val projectSettings = UnidocPlugin.unidocSettings ++ Seq(
+  override def projectSettings = UnidocPlugin.unidocSettings ++ Seq(
+    scalacOptions in (Compile, doc) ++= Seq("-unchecked", "-deprecation", "-implicits"),
     scalacOptions in(Compile, doc) ++= Opts.doc.title(scrupalTitle.value),
-    scalacOptions in(Compile, doc) ++= Opts.doc.version(version.value)
+    scalacOptions in(Compile, doc) ++= Opts.doc.version(version.value),
+    apiURL := Some(url("http://scrupal.org/modules/" + normalizedName.value + "/api/")),
+    autoAPIMappings := true,
+    apiMappings ++= {
+      val cp: Seq[Attributed[File]] = (fullClasspath in Compile).value
+      def findManagedDependency(organization: String, name: String): File = {
+        ( for {
+          entry <- cp
+          module <- entry.get(moduleID.key)
+          if module.organization == organization
+          if module.name.startsWith(name)
+          jarFile = entry.data
+        } yield jarFile
+          ).head
+      }
+      Map(
+        findManagedDependency("org.scala-lang", "scala-library") → url(s"http://www.scala-lang.org/api/$scalaVersion/"),
+        findManagedDependency("com.typesafe.akka", "akka-actor") → url(s"http://doc.akka.io/api/akka/"),
+        findManagedDependency("com.typesafe", "config") → url("http://typesafehub.github.io/config/latest/api/"),
+        findManagedDependency("joda-time", "joda-time") → url("http://joda-time.sourceforge.net/apidocs/")
+      )
+    }
   )
 }
 
 object Site extends PluginSettings {
   import com.typesafe.sbt.SbtSite
-  override lazy val projectSettings = SbtSite.settings
+  override def projectSettings = SbtSite.settings
 }
 
-object GhPages {
+object GhPages extends PluginSettings {
   import com.typesafe.sbt.SbtGhPages
 
-  lazy val buildSettings = SbtGhPages.buildSettings
-  lazy val projectSettings = SbtGhPages.projectSettings
-  lazy val globalSettings = SbtGhPages.globalSettings
+  override def buildSettings = SbtGhPages.buildSettings
+  override def projectSettings = SbtGhPages.projectSettings
+  override def globalSettings = SbtGhPages.globalSettings
 }
